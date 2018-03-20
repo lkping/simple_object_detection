297e473db1e7a752113faa4e1edbe4f7b49a688d
kaiping.liu <kaiping.liu@husor.com>
2018-03-20 16:15:07 +0800

    first commit

---
+++ check_env.py
@@ -0,0 +1,27 @@
+import numpy as np  
+import os  
+import six.moves.urllib as urllib  
+import sys  
+import tarfile  
+import tensorflow as tf  
+import zipfile  
+  
+from collections import defaultdict  
+from io import StringIO  
+from matplotlib import pyplot as plt  
+from PIL import Image  
+
+if tf.__version__ < '1.4.0':  
+  raise ImportError('Please upgrade your tensorflow installation to v1.4.* or later!')  
+
+# This is needed to display the images.  
+%matplotlib inline  
+  
+# This is needed since the notebook is stored in the object_detection folder.  
+sys.path.append("..")  
+
+from utils import label_map_util  
+  
+from utils import visualization_utils as vis_util  
+
+print("ok");
